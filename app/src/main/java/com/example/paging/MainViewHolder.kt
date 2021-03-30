14195c66b3d7a64c3df45057ec2a55968fba85d1
오준영 <ojun-yeong@ojun-yeongs-MacBook-Pro.local>
2021-03-30 21:28:05 +0900

    Add images

--- app/src/main/java/com/example/paging/MainViewHolder.kt
+++ app/src/main/java/com/example/paging/MainViewHolder.kt
@@ -3,9 +3,16 @@ package com.example.paging
 import androidx.recyclerview.widget.RecyclerView
 import com.example.paging.databinding.MonItemBinding
 
-class MainViewHolder(val monBinding: MonItemBinding) :
-  RecyclerView.ViewHolder(monBinding.root) {
+class MainViewHolder(val binding: MonItemBinding) :
+  RecyclerView.ViewHolder(binding.root) {
   fun setData(data: Result) {
-    monBinding.monTextView.text = data.name
+    val tokens = if (data.url.endsWith("/")) {
+      data.url.dropLast(1).split("/")
+    } else {
+      data.url.split("/")
+    }
+    val id = tokens.last()
+    binding.imageView.setImageURI("https://pokeres.bastionbot.org/images/pokemon/$id.png")
+    binding.monTextView.text = data.name
   }
 }
